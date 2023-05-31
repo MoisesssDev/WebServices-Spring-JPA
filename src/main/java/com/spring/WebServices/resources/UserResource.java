89f338669b97b40c49da395a067a6a0f2bb603fc
[Moises Almeida] <[moisesssprg@gmail.com]>
2023-05-31 17:26:59 -0300

    user entity and resource

---
+++ src/main/java/com/spring/WebServices/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.spring.WebServices.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.WebServices.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Joao", "joaozin@gmail.com", "12345678", "32323232");
+		return ResponseEntity.ok().body(u);
+	}
+}
